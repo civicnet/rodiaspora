c3a2315c01b56b72fd081eb1fa7c4cc9bc97f97f
ClaudiuCeia <claudiu.ceia+1@gmail.com>
2019-01-16 00:16:55 +0200

    (fix) async load images

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import {
   AppLoading,
   Font,
   Icon,
+  Asset,
 } from 'expo';
 
 import { Root } from 'native-base';
@@ -15,19 +16,43 @@ export default class App extends React.Component {
     isLoadingComplete: false,
   };
 
-  _loadResourcesAsync = async () => Promise.all([
-    Font.loadAsync({
-      ...Icon.Ionicons.font,
-      /* eslint-disable */
-      Roboto: require('native-base/Fonts/Roboto.ttf'),
-      RobotoSlab: require('./assets/fonts/RobotoSlab-Regular.ttf'),
-      RobotoSlab_light: require('./assets/fonts/RobotoSlab-Light.ttf'),
-      RobotoSlab_bold: require('./assets/fonts/RobotoSlab-Bold.ttf'),
-      RobotoSlab_thin: require('./assets/fonts/RobotoSlab-Thin.ttf'),
-      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
-      /* eslint-enable */
-    }),
-  ]);
+  _loadResourcesAsync = async () => {
+    const images = [
+      require('./assets/images/gears.png'),
+      require('./assets/images/icon.png'),
+      require('./assets/images/logo-civictech-small.png'),
+      require('./assets/images/logo-mae.png'),
+      require('./assets/images/not-found.png'),
+      require('./assets/images/passport.png'),
+      require('./assets/images/placeholder.png'),
+      require('./assets/images/tower.png'),
+      require('./assets/images/user-location-pin.png'),
+      require('./assets/images/vote-o-black.png'),
+      require('./assets/images/vote-o-green.png'),
+      require('./assets/images/vote-s-yellow.png'),
+      require('./assets/images/vote-s-yellow-alt.png'),
+      require('./assets/images/warning.png'),
+    ];
+
+    const cacheImages = images.map((image) => {
+      return Asset.fromModule(image).downloadAsync();
+    });
+
+    return Promise.all([
+      ...cacheImages,
+      Font.loadAsync({
+        ...Icon.Ionicons.font,
+        /* eslint-disable */
+        Roboto: require('native-base/Fonts/Roboto.ttf'),
+        RobotoSlab: require('./assets/fonts/RobotoSlab-Regular.ttf'),
+        RobotoSlab_light: require('./assets/fonts/RobotoSlab-Light.ttf'),
+        RobotoSlab_bold: require('./assets/fonts/RobotoSlab-Bold.ttf'),
+        RobotoSlab_thin: require('./assets/fonts/RobotoSlab-Thin.ttf'),
+        Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+        /* eslint-enable */
+      }),
+    ]);
+  }
 
   _handleLoadingError = (error) => {
     // In this case, you might want to report the error to your error
